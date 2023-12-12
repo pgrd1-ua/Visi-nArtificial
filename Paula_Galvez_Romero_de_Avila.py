f41a8f7896117fc96bfc90bc1bbd8fb99cc33042
Paula <pgrd1@gcloud.ua.es>
2023-12-13 00:14:43 +0100

    Tareas 1A 1B y 1C hechas

--- Paula_Galvez_Romero_de_Avila.py
+++ Paula_Galvez_Romero_de_Avila.py
@@ -7,128 +7,69 @@ import time
 class DecisionStump:
     def __init__(self, n_caracteristicas):
         self.caracteristica_index = np.random.randint(0, n_caracteristicas) # Seleccionar una característica al azar. n_caracteristicas es el número total de características disponibles.
-        self.umbral = None # Inicializar el umbral a None. Este será determinado durante el entrenamiento.
+        
+        self.umbral = np.random.uniform() # Inicializar el umbral a None. Este será determinado durante el entrenamiento.
         self.polaridad = np.random.choice([1, -1]) # Polaridad puede ser 1 o -1, se elige al azar.
         self.alpha = None # Valor de alpha (peso del clasificador), inicializado a None. Este será calculado durante el entrenamiento.
     
     def predict(self, X):
-        predicciones = np.ones(X.shape[0]) # Inicializar todas las predicciones a 1.
-        if self.polaridad == 1: # Dependiendo de la polaridad, ajustar las predicciones basadas en el umbral y la característica seleccionada.
-            predicciones[X[:, self.caracteristica_index] < self.umbral] = -1  # Si la característica es menor que el umbral, la predicción es -1.
+        n_samples = X.shape[0]
+        predicciones = np.ones(n_samples)
+        feature_column = X[:, self.caracteristica_index]
+        if self.polaridad == 1:
+            predicciones[feature_column < self.umbral] = -1
         else:
-            predicciones[X[:, self.caracteristica_index] >= self.umbral] = 1 # Si la característica es mayor que el umbral, la predicción es -1.
+            predicciones[feature_column >= self.umbral] = -1
         return predicciones
 
+
 class Adaboost:
-    ## Constructor de clase, con número de clasificadores e intentos por clasificador
     def __init__(self, T=5, A=20):
-        # Dar valores a los parámetros del clasificador e iniciar la lista de clasificadores débiles vacía
-        self.T = T  # Número de clasificadores
-        self.A = A  # Intentos por clasificador para encontrar el mejor umbral
-        self.lista_clasificadores = []  # Lista para mantener todos los clasificadores débiles    
-        
-    ## Método para entrenar un clasificador fuerte a partir de clasificadores débiles mediante Adaboost
+        self.T = T
+        self.A = A
+        self.lista_clasificadores = []
+
     def fit(self, X, Y, verbose=False):
-        # (número_de_imágenes, 28, 28)
-        print("Obtener el número de observaciones y de características por observación de X")
-        n_samples, n_caracteristicas = X.shape 
-                                                # n_samples número total de imágenes en el conjunto de datos
-                                                # n_caracterisitcas n pixeles(cada píxel es una característica) de cada imagen
-        print("Iniciar pesos de las observaciones a 1/n_samples")
-        w = np.full(n_samples, (1 / n_samples)) 
-        #w : array de pesos. Cada elemento de w representa el peso inicial de cada imagen en tu conjunto de datos.
-        print("n_samples: ", n_samples, " y n_caracteristicas: ", n_caracteristicas)
-        print("w", w)
+        n_samples, n_caracteristicas = X.shape
+        D = np.full(n_samples, (1 / n_samples))
 
-        # Bucle de entrenamiento Adaboost # Bucle de entrenamiento Adaboost: desde 1 hasta T repetir
-        print()
-        for t in range(1, self.T): ## Hasta T clasificadores
-            print("CLASIFICADOR T: ", t)
-            print()
-            print("Iniciar el clasificador con el mayor error posible para asegurar que se actualice en el primer intento")
-            clf = DecisionStump(n_caracteristicas)
+        for t in range(self.T):
+            clf_best = None
             min_error = float('inf')
-            print(clf, min_error)
 
-            # Buscar el mejor clasificador débil: desde 1 hasta A repetir
-            # Calcular predicciones de ese clasificador para todas las observaciones
-            for a in range(1, self.A):
-                print("INTENTO A: ", a)
-                print("Generar un umbral y característica aleatorios para el clasificador")
-                caracteristica_index = np.random.randint(0, n_caracteristicas) # Crear un nuevo clasificador débil aleatorio
-                umbral = np.random.uniform(np.min(X[:, caracteristica_index]), np.max(X[:, caracteristica_index])) 
-                polaridad = np.random.choice([1, -1])
-                print("caracteristica_index: ",caracteristica_index)
-                print("umbral: ", umbral)
-                print("polaridad: ", polaridad)
+            for a in range(self.A):
+                clf = DecisionStump(n_caracteristicas)
+                # Randomly select feature and threshold
+                clf.caracteristica_index = np.random.randint(0, n_caracteristicas)
+                clf.umbral = np.random.uniform()
+                clf.polaridad = np.random.choice([1, -1])
 
-                print("Actualizar el clasificador débil con estos parámetros aleatorios")
-                clf.caracteristica_index = caracteristica_index
-                clf.umbral = umbral
-                clf.polaridad = polaridad
-
-                print()
-                print("Realizamos predicciones con el clasificador débil actual")
                 predicciones = clf.predict(X)
-                print("predicciones: ", predicciones)
-                
-                print()
-                print("Calcular el error ponderado de las predicciones comparar predicciones con los valores deseados") 
-                print(" Y acumular los pesos de las observaciones mal clasificadas")
-                error = np.sum(w[Y != predicciones])
-                print("error: ", error)
-                print("min_error: ", min_error)
+                error = np.sum(D[Y != predicciones])
 
-                print()
-                print("Actualizamos mejor clasificador hasta el momento: el que tenga menor error")
-                # Si el error es menor que el error mínimo, actualizar el clasificador y el error mínimo
                 if error < min_error:
-                    print("error es menor a min_error -> SE ACTUALIZA")
                     min_error = error
-                    best_clf = clf
-                    print("Nuevo min_error: ", min_error)
-                    print("NUEVO best_clf: ", best_clf)
-                    print("Actualizar el umbral y la polaridad del mejor clf ")
-                    best_clf.umbral = np.random.uniform(np.min(X[:, clf.caracteristica_index]), np.max(X[:, clf.caracteristica_index]))
-                    best_clf.polaridad = np.random.choice([1, -1])
-                    print("best_clf.umbral: ", best_clf.umbral, " best_clf.polaridad: ", best_clf.polaridad)
-                else:
-                    print("error es mayor a min_error -> NO SE ACTUALIZA")
+                    clf_best = clf
 
-            # if min_error == 0:
-            #     min_error = 1e-10  # Para evitar división por cero en el cálculo de alpha
+            # Calculate alpha
+            clf_best.alpha = 0.5 * np.log((1.0 - min_error) / (min_error + 1e-10))
 
-            print()
-            print("Calculamos el valor de alfa y las predicciones del mejor clasificador débil")
-            #best_clf.alpha = 0.5 * np.log((1 - min_error) / min_error)
-            best_clf.alpha = 0.5 * np.log((1 - min_error) / (min_error + 1e-10))
-            print("best_clf.alpha: ", best_clf.alpha)
+            # Update weights
+            D *= np.exp(-clf_best.alpha * Y * clf_best.predict(X))
+            D /= np.sum(D)
 
-            print()
-            print("Guardamos el mejor clasificador en la lista de clasificadores de Adaboost")
-            self.lista_clasificadores.append(best_clf)
-            print("lista_clasificadores: ", self.lista_clasificadores)
-            
-            print()
-            print("Actualizamos los pesos de las observaciones en función de las predicciones, los valores deseados y alfa")
-            print("Y Normalizamos a 1 los pesos")
-            predicciones = best_clf.predict(X)
-            print("predicciones: ", predicciones)
-            w *= np.exp(-best_clf.alpha * Y * predicciones)
-            w /= np.sum(w)
-            print("w: ", w)
-            print()
+            self.lista_clasificadores.append(clf_best)
 
             if verbose:
-                print(f'Ronda {t+1}, Caracteristica {best_clf.caracteristica_index}, Umbral {best_clf.umbral}, Polaridad {best_clf.polaridad}, Error {min_error}, Alpha {best_clf.alpha}')
-            
-            print()
+                print(f'Ronda {t+1}, Caracteristica {clf_best.caracteristica_index}, '
+                    f'Umbral {clf_best.umbral}, Polaridad {clf_best.polaridad}, '
+                    f'Error {min_error}, Alpha {clf_best.alpha}')
 
-    # Método para obtener una predicción con el clasificador fuerte Adaboost 
     def predict(self, X):
-        clf_preds = [clf.alpha * clf.predict(X) for clf in self.lista_clasificadores] # Calcular las predicciones de cada clasificador débil para cada input multiplicadas por su alfa
-        y_pred = np.sum(clf_preds, axis=0) # Sumar para cada input todas las predicciones ponderadas y decidir la clase en función del signo
+        clf_preds = [clf.alpha * clf.predict(X) for clf in self.lista_clasificadores]
+        y_pred = np.sum(clf_preds, axis=0)
         return np.sign(y_pred)
+
 
 ################################################################ Tarea 1B #########################################################################
 
@@ -157,7 +98,6 @@ def entrenamiento(digito, T, A, verbose=False):
     y_test_pred = clf.predict(X_test)
     train_accuracy = accuracy_score(y_train_digito, y_train_pred)
     test_accuracy = accuracy_score(y_test_digito, y_test_pred)
-    #accuracy es precision / exactitud
     
     # Imprimir las tasas de acierto
     print(f"Entrenando clasificador Adaboost para el dígito {digito}, T={T}, A={A}")
@@ -167,28 +107,15 @@ def entrenamiento(digito, T, A, verbose=False):
 
     print(f"Tasas acierto (train, test) y tiempo: {train_accuracy*100:.2f}%, {test_accuracy*100:.2f}%, {end_time - start_time:.3f} s")
 
-    # Devolver precisión y tiempo
     return train_accuracy, test_accuracy, end_time - start_time
 
 ################################################################ Tarea 1C #########################################################################
 
 
-def generarGraficasPlot(T_values, A_values, train_accuracies, test_accuracies, training_times):
+def generarGraficas(T_values, A_values, train_accuracies, test_accuracies, training_times):
+    # Asegúrate de que TA_producto tenga la misma longitud que las otras listas
+    TA_producto = [T * A for T, A in zip(T_values, A_values)]
     
-    # train_accuracies = []
-    # test_accuracies = []
-    # times = []
-    TA_producto = [T * A for T in T_values for A in A_values]
-
-    # # Prueba con diferentes valores de T y A y guarda los resultados
-    # for T in T_values:
-    #     for A in A_values:
-    #         train_accuracy, test_accuracy, training_time = entrenamiento(digito, T, A, True) # Si decido descomentar esta linea hay que añadir por parametro a digito
-    #         train_accuracies.append(train_accuracy)
-    #         test_accuracies.append(test_accuracy)
-    #         times.append(training_time)
-
-    # Ahora, crea la gráfica de doble eje
     fig, ax1 = plt.subplots()
 
     ax1.set_xlabel('T x A')
@@ -207,12 +134,17 @@ def generarGraficasPlot(T_values, A_values, train_accuracies, test_accuracies, t
     fig.tight_layout()
     plt.title('Accuracy and Training Time vs T x A')
     plt.show()
-    plt.close()  # Cierra la ventana de la gráfica
+    plt.close()
+
 
 def main():
+    
+    # train_accuracy, test_accuracy, training_time = entrenamiento(9, 10, 20)
+
     # Define los rangos de valores para T y A que deseas probar
-    T_values = [10, 5, 10]# 10, 20, 5, 30]
-    A_values = [20, 10, 30]# 40, 40, 30, 30] 
+    #T_values = [10, 10, 10, 10]# 10, 20, 5, 30]
+    T_values = [10, 20, 30, 40]
+    A_values = [5, 10, 20, 30]# 40, 40, 30, 30] 
     
     digitos = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     
@@ -221,32 +153,18 @@ def main():
         test_accuracies_matrizAux = []
         training_times_matrizAux = []
         print("DIGITO: ", digito)
-        for T in T_values:
-            for A in A_values:
-                print()
-                print("Clasificador T:", T, " intento : ", A)
-                print()
-                train_accuracy, test_accuracy, training_time = entrenamiento(digito, T, A)
-                print()
-                print("train_accuracy: ", train_accuracy)
-                print("test_accuracy: ", test_accuracy)
-                print("training_time: ", training_time)
-                train_accuracies_matrizAux.append(train_accuracy)
-                test_accuracies_matrizAux.append(test_accuracy)
-                training_times_matrizAux.append(training_time)
-                print()
-                print("train_accuracies_matrizAux ", train_accuracies_matrizAux)
-                print("test_accuracies_matrizAux ", test_accuracies_matrizAux)
-                print("training_times_matrizAux ", training_times_matrizAux)
-        print()
-        print("Fin del entrenamiento para el dígito ", digito)
-        generarGraficasPlot(T_values, A_values, train_accuracies_matrizAux, test_accuracies_matrizAux, training_times_matrizAux)
-        print("#### GRAFICA ####")
-        print()
+
+        for T, A in zip(T_values, A_values):
+            train_accuracy, test_accuracy, training_time = entrenamiento(digito, T, A)
+            
+            train_accuracies_matrizAux.append(train_accuracy)
+            test_accuracies_matrizAux.append(test_accuracy)
+            training_times_matrizAux.append(training_time)
+        
+        # Generar y mostrar gráficas después de procesar todos los valores de T y A para un dígito
+        generarGraficas(T_values, A_values, train_accuracies_matrizAux, test_accuracies_matrizAux, training_times_matrizAux)
+
 
 
 if __name__ == "__main__":
     main()
-
-    
-    
